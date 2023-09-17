84f28eeea64ef59b05e17e6f4632e3a08f6fc8c9
BraiderZ <luisjbc2112@gmail.com>
2023-09-17 14:20:04 -0600

    EL script verifica que se coloque un solo argumento y que exista el proceso

--- ScriptingyProcesos1.sh
+++ ScriptingyProcesos1.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+
+
+
+if [ $# -lt 1 ]; then
+	echo "Error: agregue un PID como argumento."
+	exit 1
+elif [ $# -gt 1 ]; then
+        echo "Error: solo puede agregar un PID a la vez."
+        exit 1
+fi
+
+
+if ps -p $1 > /dev/null; then
+	nombre_proceso=$(ps -o comm= -p $1)
+	echo "Nombre del proceso: $nombre_proceso"
+else
+	echo "EL proceso no existe"
+fi
+
