ca1769750ecd89a5b51121d9b9b0a158b1ee8973
BraiderZ <luisjbc2112@gmail.com>
2023-09-17 14:23:55 -0600

    EL script devuelve el nombre e PID del proceso

--- ScriptingyProcesos1.sh
+++ ScriptingyProcesos1.sh
@@ -15,6 +15,9 @@ fi
 if ps -p $1 > /dev/null; then
 	nombre_proceso=$(ps -o comm= -p $1)
 	echo "Nombre del proceso: $nombre_proceso"
+	id_proceso=$(ps -o pid= -p $1)
+        echo "PID del proceso: $id_proceso"
+
 else
 	echo "EL proceso no existe"
 fi
