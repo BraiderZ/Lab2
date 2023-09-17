06436e1ac3d1b4c457d94a837b272a7de59293ca
BraiderZ <luisjbc2112@gmail.com>
2023-09-17 16:03:47 -0600

    Valorando caso con comandos que necesiten mas argumentos

--- ScriptingyProcesos2.sh
+++ ScriptingyProcesos2.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
+monitoreo () {
 
+	while true; do
+		if pgrep -x $1 > /dev/null; then
+			echo "$1 se esta ejecutando"
+		else
+			echo "$1 no se esta ejecutando. Ejecutando proceso en background..."
+		fi
+		break
+	done
+}
+
+if [ $# -lt 2 ]; then
+	echo "Error: agregue almenos  dos argumentos: <nombre del proceso> <comando para ejecutarlo>"
+	exit 1
+fi
+
+proceso=$1
+comando=$2
+shift 1
+shift 2
+resto_comando=$@
+
+monitoreo "$proceso" "$comando" "$resto_comando"
