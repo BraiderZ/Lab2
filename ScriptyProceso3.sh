a27f2b8b36a3db24cb32e035ab8f2ef00ca6bf8f
BraiderZ <luisjbc2112@gmail.com>
2023-09-17 18:27:48 -0600

    El script revisa que se haya colocado almenos un argumento y verifica si existe el ejecutable

--- ScriptyProceso3.sh
+++ ScriptyProceso3.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+if [ $# -lt 1 ]; then
+	echo "Agregue como argumento un ejecutable"
+	exit 1
+fi
+
+ejecutable=$@
+
+if [ -x "$ejecutable" ]; then
+	echo "Este archivo es ejecutable"
+else
+	echo "ESte archivo no es ejecutable"
+	exit 1
+fi
+
