1e1485f3d82a1c032cd9b179d6046e1cd59becb1
BraiderZ <luisjbc2112@gmail.com>
2023-09-19 13:20:34 -0600

    modificaciones finales

--- servicio_directorio.sh
+++ servicio_directorio.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-log="registro_directorio.log"
+log="/home/luis/Lab2/Lab2/registro_directorio.log"
 echo "Tipo de cambio Fecha Hora" > "$log"
 while true; do
-  evento=$(inotifywait -q -e modify,create,delete,move ~/prueba_scriptyservicios)
-  fecha_hora=$(date +"%Y-%m-%d %H:%M:%S")
-  echo "$evento $fecha_hora" >> "$log"
+    evento=$(inotifywait -q -e modify,create,delete,move -t 0 ~/prueba_scriptyservicios)
+  
+  # Verificar si se ha producido un evento antes de registrar
+  if [ -n "$evento" ]; then
+    fecha_hora=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "$evento $fecha_hora" >> "$log"
+  fi
 done
