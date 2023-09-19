c10aa5862369e4bc8d4ab20f0b674e79ff0afe92
BraiderZ <luisjbc2112@gmail.com>
2023-09-19 08:57:24 -0600

    Codigo del script terminado

--- servicio_directorio.sh
+++ servicio_directorio.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-
+log="registro_directorio.log"
+echo "Tipo de cambio Fecha Hora" > "$log"
 while true; do
   evento=$(inotifywait -q -e modify,create,delete,move ~/prueba_scriptyservicios)
   fecha_hora=$(date +"%Y-%m-%d %H:%M:%S")
-  echo "Cambio detectado en $evento en $fecha_hora"
+  echo "$evento $fecha_hora" >> "$log"
 done
