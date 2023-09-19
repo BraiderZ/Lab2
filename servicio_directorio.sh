ed3b96fb59827110e90d04cd5ecd5af4c6458268
BraiderZ <luisjbc2112@gmail.com>
2023-09-19 08:48:33 -0600

    El script imprime el cambio realizado en el directorio elegido

--- servicio_directorio.sh
+++ servicio_directorio.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "hello"
+
+while true; do
+  evento=$(inotifywait -q -e modify,create,delete,move ~/prueba_scriptyservicios)
+  fecha_hora=$(date +"%Y-%m-%d %H:%M:%S")
+  echo "Cambio detectado en $evento en $fecha_hora"
+done
